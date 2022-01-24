96c3214b787c8c2d0a8de5815962549ac87b622e
Victor D. Lopez <vlopez0392@gmail.com>
2022-01-25 00:17:12 +0800

    Improves input validation demo

--- 6_Character_I0/06_input_validation/inputvalidation_ver1.c
+++ 6_Character_I0/06_input_validation/inputvalidation_ver1.c
@@ -1,15 +1,33 @@
 /*Input validation Ver 1
 * This program simply prints non-negative numbers. Its main purpose is to
 * handle user input errors with grace.
+*
+* Comments: If the user inputs something other than a number, the program lets the user try again.
 */
 #include <stdio.h>
 int main(void){
-    long n; 
-    scanf("%ld", &n);  //Get first value
-    while(n>=0){       //Detect an out-of-range value 
-        printf("Enter a number: ");
-        printf("You input the following number %ld \n", n);
-        scanf("%ld", &n);
+    long n = 1; 
+    char ch;
+    while(n>=0){  //Detect an out-of-range value 
+        printf("Enter a number or press q to quit: ");
+        if(scanf("%ld", &n) == 1){
+            printf("You have input: %ld \n", n);
+            continue;
+        }else if((ch = getchar())== 'q'){
+            if(getchar() != '\n'){
+                while(getchar()!= '\n');
+                printf("I only understand q to quit, please try again \n");
+                continue;
+            }
+            printf("Terminating program, bye!");
+            break;
+        }else{
+            putchar(ch);
+            while((ch = getchar()) != '\n'){
+                putchar(ch);
+            }
+            printf(" is not a number Please try again \n");
+        }
     }
     return 0;
 }
