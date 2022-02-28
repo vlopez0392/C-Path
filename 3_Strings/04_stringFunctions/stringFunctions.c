372986fb360cda037a4fee33e3fcf8637601f672
Victor D. Lopez <vlopez0392@gmail.com>
2022-02-28 23:19:17 +0800

    Added stringFunctions.c

--- 3_Strings/04_stringFunctions/stringFunctions.c
+++ 3_Strings/04_stringFunctions/stringFunctions.c
@@ -2,27 +2,39 @@
 #include <string.h>
 
 int main(){
+    char myString[] = "My name is Vick";
+    
+    //String length 
+    printf("The length of this string is: %lu \n", strlen(myString));
 
     //String copy
-    char myString[] = "My name is Jason";
+    //////strncpy() 
     char temp[50];
-
     strncpy(temp, myString, sizeof(temp)-1); //First parameter is destination, second parameter is string source
                                              //Third parameter is the maximum number fo characters to copy
     printf("temp now holds the value of: %s \n", temp);
-
-    //String Concatenation
+    
+    /////strcpy()
     char src[50], dest[50];
-
-    strcpy(src, "This is source");
+    strcpy(src, " This is source, This is source");
     strcpy(dest, "This is destination");
+   
+    //String Concatenation
+    ////Example 1
+    strncat(dest, src, 10);
+    printf("Final destination string: |%s|: ",dest);
 
-    strncat(dest, src, 15);
-    printf("Final destination string: |%s|: " ,dest);
+    ////Example 2
+    char anotherString[] = " my string"; 
+    char input[80]; 
+
+    printf("\nEnter a string to be concatenated: ");
+    scanf("%s", input); 
+
+    printf("\nThe string %s to be concatenated with %s is: ", anotherString, input);
+    printf("%s", strcat(input, anotherString));
 
     //String comparison - Compares string contents
-    
-
 
     return 0;
 }
